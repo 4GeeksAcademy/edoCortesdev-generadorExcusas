a43089e981c78336facb3fc7e8eacf31b931f2b5
edoCortesDev <edo.cortes.rojas@gmail.com>
2024-12-18 23:15:38 +0000

    Trabajo finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let sujeto = ["el perro", "mi abuela", "el cartero", "mi pájaro"];
+  let accion = ["se comió", "orinó en", "aplastó", "rompió"];
+  let objeto = ["mi tarea", "mi teléfono", "el coche"];
+  let momento = [
+    "antes de la clase",
+    "cuando estaba durmiendo",
+    "mientras hacía ejercicio",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+
+  function generarExcusa() {
+    let sujetoAleatorio = sujeto[Math.floor(Math.random() * sujeto.length)];
+    let accionAleatoria = accion[Math.floor(Math.random() * accion.length)];
+    let objetoAleatorio = objeto[Math.floor(Math.random() * objeto.length)];
+    let momentoAleatorio = momento[Math.floor(Math.random() * momento.length)];
+
+    let excusa =
+      sujetoAleatorio +
+      " " +
+      accionAleatoria +
+      " " +
+      objetoAleatorio +
+      " " +
+      momentoAleatorio;
+
+    document.getElementById("excusa").innerText = excusa;
+  }
+
+  generarExcusa();
+
   console.log("Hello Rigo from the console!");
 };
